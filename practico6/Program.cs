e94ce1a00dff7ed194eb73741eaa3c5346d6ea1c
yaz moyano <moyanoyazminalejandra@gmail.com>
2023-05-18 19:50:09 -0300

    Segundo commit, commienzo punto 1

---
+++ practico6/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a = 10;
+b = a;
+
+Console.WriteLine("valor de a: " +a);
+Console.WriteLine("valor de b: " +b);
+
+float floatNum1= 0;
+bool anda = false;
+string? numString = "";
+
+while (!anda)
+{
+    Console.WriteLine("Ingrese un numero válido");
+    numString = Console.ReadLine();
+    anda = float.TryParse(numString, out floatNum1);
+    if (!anda) {
+        Console.WriteLine(numString + " no es numero válido.");   
+    }
+
+}
+
+if (floatNum1 > 0 )
+{
+    while (aux)
+    {
+        
+    }
+}
