6d8b2a19b4b4c4f2a08aaa46558b2849659fd6da
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-05-19 02:50:42 -0300

    Tercer commit, ejer 1 terminado.

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -7,10 +7,10 @@ int b;
 a = 10;
 b = a;
 
-Console.WriteLine("valor de a: " +a);
-Console.WriteLine("valor de b: " +b);
+Console.WriteLine("valor de a: " + a);
+Console.WriteLine("valor de b: " + b);
 
-float floatNum1= 0;
+int intNum1= 0;
 bool anda = false;
 string? numString = "";
 
@@ -18,17 +18,34 @@ while (!anda)
 {
     Console.WriteLine("Ingrese un numero válido");
     numString = Console.ReadLine();
-    anda = float.TryParse(numString, out floatNum1);
+    anda = int.TryParse(numString, out intNum1);
     if (!anda) {
         Console.WriteLine(numString + " no es numero válido.");   
     }
 
 }
+int aux;
+int resto;
+int cantDigitos = 0;
+double invertido = 0;
+aux = intNum1;
 
-if (floatNum1 > 0 )
+ while(aux != 0){             
+    aux = aux/10;         
+    cantDigitos++;      
+}
+
+aux = intNum1;
+
+if (intNum1 > 0 )
 {
-    while (aux)
+    while (aux != 0)
     {
-        
+        resto = aux % 10;
+        invertido += (resto * Math.Pow(10,cantDigitos-1));
+        aux = aux / 10;
+        cantDigitos = cantDigitos - 1;
     }
 }
+
+Console.WriteLine("El numero invertido es: " + invertido);
