835adcf01dc96fda652e258c3612ff7172842d5a
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-05-30 17:04:53 -0300

    Ejercicio 2 Corregido

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,47 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//int intNum1= 0;
 bool anda = false;
 string? numString = "";
-/*
-while (!anda)
-{
-    Console.WriteLine("Ingrese un numero válido");
-    numString = Console.ReadLine();
-    anda = int.TryParse(numString, out intNum1);
-    if (!anda) {
-        Console.WriteLine(numString + " no es numero válido.");   
-    }
-}
 
-int aux;
-int resto;
-int cantDigitos = 0;
-double invertido = 0;
-aux = intNum1;
-
- while(aux != 0){             
-    aux = aux/10;         
-    cantDigitos++;      
-}
-
-aux = intNum1;
-
-if (intNum1 > 0 )
-{
-    while (aux != 0)
-    {
-        resto = aux % 10;
-        invertido += (resto * Math.Pow(10,cantDigitos-1));
-        aux = aux / 10;
-        cantDigitos = cantDigitos - 1;
-    }
-} 
-
-Console.WriteLine("El numero invertido es: " + invertido);
- */
-
-//Calculadora
+//Calculadora V1
 
 int num1=0, num2=0, operacion=0, resultado = 0;
 string? Aux = "";
@@ -119,7 +81,7 @@ do
             }
         }
     }
-    
+
     anda = false;
     //Control para continuar operando
     while (!anda)
